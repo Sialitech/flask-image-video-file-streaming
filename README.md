633e25fe7d0cf46cc796f4c8914ba0249cbc8f45
betegon <miguelbetegongarcia@gmail.com>
2021-01-25 11:22:31 +0100

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-flask-video-streaming
-=====================
+# flask-image-video-file-streaming
 
-Supporting code for my article [video streaming with Flask](http://blog.miguelgrinberg.com/post/video-streaming-with-flask) and its follow-up [Flask Video Streaming Revisited](http://blog.miguelgrinberg.com/post/flask-video-streaming-revisited).
+1. run `pip install -r requirements-opencv.txt`
+2. Change port in `app.py`.
+3. Place images/video in a folder.
+4. create environment variables:
+   1. `VOLUME_PATH` path to files folder.
+   2. `SRC` value should be `"images"` or `"video"`.
+5. run `python app.py`
+
+## CREDIT TO
+
+ Article [video streaming with Flask](http://blog.miguelgrinberg.com/post/video-streaming-with-flask) and its follow-up [Flask Video Streaming Revisited](http://blog.miguelgrinberg.com/post/flask-video-streaming-revisited).
--- camera.py
+++ camera.py
@@ -1,20 +1,22 @@
 import time
 import glob
 from base_camera import BaseCamera
-
-
+import os
+import cv2
 class Camera(BaseCamera):
     """An emulated camera implementation that streams a repeated sequence of
     files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
-
-    imgs = set_imgs("volume")
-
-    def set_imgs(path):
-        return [img for img in glob.glob("{}/*".format(path))]
-
+    # imgs = [open(img, 'rb').read() for img in glob.glob("{}/*".format(os.environ['VOLUME_PATH']))]
+    videos = [video for video in glob.glob("{}/*".format(os.environ['VOLUME_PATH']))]
+    print(videos[0])
     @staticmethod
     def frames():
+        cam = cv2.VideoCapture("/flask-image-video-file-streaming/files/Friends.mp4")
         while True:
-            for img in Camera.imgs:
-                time.sleep(1)
-                yield img
+            success, img = cam.read()
+            print(img)
+            print(success)
+            print("SUCCESS")
+            img_encoded = cv2.imencode('.jpg', img)[1]
+            img_bytes = img_encoded.tobytes()
+            yield img_bytes
